882a437ccec425c6015403f922d8630d24cb0257
Mateusz <peczek.mateusz@gmail.com>
2017-02-14 19:36:54 +0100

    VehicleDetail model refactor, vehicle controller refactor

--- src/FuelTracker/ApiModels/VehicleApiModels/VehicleDetailsModel.cs
+++ src/FuelTracker/ApiModels/VehicleApiModels/VehicleDetailsModel.cs
@@ -6,11 +6,24 @@ using System.Threading.Tasks;
 
 namespace FuelTracker.ApiModels.VehicleApiModels
 {
-    public class VehicleDataModel
+    public class VehicleDetailsModel
     {
         public string Manufacturer { get; set; }
+
         public string Model { get; set; }
+
         public int ProductionYear { get; set; }
-        public EngineDataModel Engine { get; set; }
+
+        public string EngineName { get; set; }
+
+        public int Power { get; set; }
+
+        public int Torque { get; set; }
+
+        public int Cylinders { get; set; }
+
+        public float Displacement { get; set; }
+
+        public FuelType FuelType { get; set; }
     }
 }
--- src/FuelTracker/Controllers/VehicleController.cs
+++ src/FuelTracker/Controllers/VehicleController.cs
@@ -20,9 +20,9 @@ namespace FuelTracker.Controllers
     {
         private readonly ICommandSender commandBus;
 
-        public VehicleController(ICommandSender commandSender)
+        public VehicleController(ICommandSender commandBus)
         {
-            this.commandBus = commandSender;
+            this.commandBus = commandBus;
         }
 
         // GET: api/values
@@ -34,11 +34,11 @@ namespace FuelTracker.Controllers
 
         // GET api/values/5
         [HttpGet("{guid}")]
-        public IActionResult Get(Guid guid)
+        public ActionResult Get(Guid guid)
         {
             using (var db = new SqlConnection(@"Server=.;Database=FuelTracker;Trusted_Connection=True;MultipleActiveResultSets=true"))
             {
-                var sqlQuery = @"select mf.name as manufacturer, md.name as modelname, v.productionyear, e.name, e.power, e.torque, e.cylinders, e.displacement, e.fueltype
+                var sqlQuery = @"select mf.name as manufacturer, md.name as model, v.productionyear, e.name as enginename, e.power, e.torque, e.cylinders, e.displacement, e.fueltype
                                  from vehicle v
                                  join ModelName md on md.Id = v.ModelNameId
                                  left join Manufacturer mf on mf.Id = md.ManufacturerID
@@ -46,13 +46,7 @@ namespace FuelTracker.Controllers
                                  where v.Guid = @Guid";
 
 
-                var result = db.Query(sqlQuery, new { Guid = guid }).Select(r => new VehicleDataModel()
-                {
-                    Engine = new EngineDataModel() { Cylinders = r.cylinders, Displacement = r.displacement, FuelType = (Domain.VehicleDomain.FuelType)r.fueltype, Name = r.name, Power = r.power, Torque = r.torque },
-                    Manufacturer = r.manufacturer,
-                    Model = r.modelname,
-                    ProductionYear = r.productionyear                 
-                }).FirstOrDefault();
+                var result = db.Query<VehicleDetailsModel>(sqlQuery, new { Guid = guid }).SingleOrDefault();
 
                 if (result != null)
                     return new JsonResult(result);
