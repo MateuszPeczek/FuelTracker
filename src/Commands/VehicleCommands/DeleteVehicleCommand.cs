bc66bc66fea3526449c636bc9ffc768fb5585b9a
Mateusz <peczek.mateusz@gmail.com>
2017-02-14 21:12:38 +0100

    DeleteVehicle Command with handling

---
+++ src/Commands/VehicleCommands/DeleteVehicleCommand.cs
@@ -0,0 +1,50 @@
+﻿using Common.Interfaces;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commands.VehicleCommands
+{
+    public class DeleteVehicleCommand : ICommand
+    {
+        public DeleteVehicleCommand(Guid guid)
+        {
+            Guid = guid;
+        }
+
+        public Guid Guid { get; set; }
+    }
+
+    public class DeleteVehicleCommandHandler : ICommandHandler<DeleteVehicleCommand>
+    {
+        private readonly ApplicationContext context;
+
+        public DeleteVehicleCommandHandler(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        public void Handle(DeleteVehicleCommand command)
+        {
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                try
+                {
+                    var vehicleToDelete = context.Vehicle.Single(s => s.Guid == command.Guid);
+                    context.Entry(vehicleToDelete).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+
+                    context.SaveChanges();
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+    }
+}
+
