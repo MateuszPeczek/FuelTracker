af14a69d2bbf143f1650393c36b4f184c64e393d
Mateusz <peczek.mateusz@gmail.com>
2017-04-18 20:39:13 +0200

    UserSettings class

---
+++ src/Domain/UserDomain/UserSettings.cs
@@ -0,0 +1,17 @@
+﻿using Common.Interfaces;
+using Domain.Common;
+using Domain.VehicleDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.UserDomain
+{
+    public class UserSettings : IEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public FuelType FuelType { get; set; }
+        public Units Units { get; set; }
+    }
+}
