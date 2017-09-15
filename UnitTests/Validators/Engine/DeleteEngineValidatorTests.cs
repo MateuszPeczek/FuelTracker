8e619ccd26c7caa0b2e3787b559680e2f26ecf07
Mateusz <peczek.mateusz@gmai;.com>
2017-09-15 19:21:53 +0200

    Delete command unit tests

---
+++ UnitTests/Validators/Engine/DeleteEngineValidatorTests.cs
@@ -0,0 +1,45 @@
+﻿using Commands.EngineCommands;
+using Common.Interfaces;
+using CustomExceptions.Vehicle;
+using System;
+using UnitTests.Extensions;
+using Xunit;
+
+namespace UnitTests.Validators.Engine
+{
+    public class DeleteEngineValidatorTests
+    {
+        private readonly ICommandValidator<DeleteEngine> deleteEngineCommandValidator;
+
+        public DeleteEngineValidatorTests()
+        {
+            deleteEngineCommandValidator = new DeleteEngineValidator();
+        }
+
+        #region Setup
+
+        private DeleteEngine GetValidDeleteEngineCommand()
+        {
+            return new DeleteEngine(Guid.NewGuid());
+        }
+
+        #endregion
+
+        [Fact]
+        private void EmptyGuidValue_ThrowsInvalidEngineIdException()
+        {
+            var stubCommand = GetValidDeleteEngineCommand();
+            stubCommand.Id = new Guid();
+
+            Assert.Throws<InvalidEngineIdException>(() => deleteEngineCommandValidator.Validate(stubCommand));
+        }
+
+        [Fact]
+        private void ValidCommand_NotTrowsAnyExceptions()
+        {
+            var stubCommand = GetValidDeleteEngineCommand();
+
+            ExtendedAssert.NotThrowsAnyExceptions(() => deleteEngineCommandValidator.Validate(stubCommand));
+        }
+    }
+}
