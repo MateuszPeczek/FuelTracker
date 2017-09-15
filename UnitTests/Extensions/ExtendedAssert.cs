87827d2bccd3cf32970f12a360e0c02583b295fc
Mateusz <peczek.mateusz@gmai;.com>
2017-09-15 19:10:49 +0200

    Change test project type, create additional Assert object with check if exceptions has not been thrown, tests for AddEngine command validator

---
+++ UnitTests/Extensions/ExtendedAssert.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.Extensions
+{
+    internal static class ExtendedAssert
+    {
+        internal static void NotThrowsAnyExceptions(Action testCode)
+        {
+            try
+            {
+                testCode.Invoke();
+            }
+            catch (Exception ex)
+            {
+                var exceptionType = ex.GetType();
+                Type innerExceptionType = null;
+                var message = new StringBuilder();
+
+                if (ex.InnerException != null)
+                    innerExceptionType = ex.InnerException.GetType();
+
+                message.Append($"Throws {ex.GetType()}");
+                if (innerExceptionType != null)
+                    message.Append($" InnerException - {innerExceptionType.GetType()}");
+
+                throw new Exception(message.ToString());
+            }
+        }
+    }
+}
---
+++ UnitTests/Validators/Engine/AddEngineValidatorTests.cs
@@ -0,0 +1,61 @@
+﻿using Commands.EngineCommands;
+using Common.Interfaces;
+using CustomExceptions.Engine;
+using CustomExceptions.Vehicle;
+using Domain.VehicleDomain;
+using System;
+using UnitTests.Extensions;
+using Xunit;
+
+namespace UnitTests.Validators.Engine
+{
+    public class AddEngineValidatorTests
+    {
+        private readonly ICommandValidator<AddEngine> addEngineCommandValidator;
+
+        public AddEngineValidatorTests()
+        {
+            addEngineCommandValidator = new AddEngineValidator();
+        }
+
+        #region Setup
+
+        private AddEngine GetStubValidCommand()
+        {
+            return new AddEngine("TestEngine",
+                                 100,
+                                 200,
+                                 4,
+                                 2000,
+                                 FuelType.Diesel);    
+        }
+
+        #endregion
+
+        [Fact]
+        private void FuelTypeValueOutOfEnumRange_ThrowsInvalidFuelTypeException()
+        {
+            var stubCommand = GetStubValidCommand();
+            stubCommand.FuelType = (FuelType)3;
+            
+            Assert.Throws<FuelTypeOutOfRangeException>(() => addEngineCommandValidator.Validate(stubCommand));
+        }
+        
+        [Fact]
+        private void EmptyGuidValue_ThrowsInvalidEngineIdException()
+        {
+            var stubCommand = GetStubValidCommand();
+            stubCommand.Id = new Guid();
+
+            Assert.Throws<InvalidEngineIdException>(() => addEngineCommandValidator.Validate(stubCommand));
+        }
+
+        [Fact]
+        private void ValidCommand_DoNotThrowsException()
+        {
+            var stubCommand = GetStubValidCommand();
+            
+            ExtendedAssert.NotThrowsAnyExceptions(() => addEngineCommandValidator.Validate(stubCommand));
+        }
+    }
+}
