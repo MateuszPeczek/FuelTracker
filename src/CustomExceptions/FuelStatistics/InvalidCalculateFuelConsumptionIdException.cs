3349a2580bf574aa6a370aa65e7afd3740f133e7
Mateusz <peczek.mateusz@gmai;.com>
2017-09-15 19:59:45 +0200

    Add validation rules to CalculateFuelConsumption command with new custom exception

---
+++ src/CustomExceptions/FuelStatistics/InvalidCalculateFuelConsumptionIdException.cs
@@ -0,0 +1,17 @@
+﻿using CustomExceptions.GroupingIntefaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomExceptions.FuelStatistics
+{
+    public class InvalidCalculateFuelConsumptionIdException : Exception, IBadRequestException
+    {
+        private const string message = "Invalid fuel calculation id";
+
+        public InvalidCalculateFuelConsumptionIdException() :base(message)
+        {
+            
+        }
+    }
+}
